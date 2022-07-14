64a20961337c1e75ee933c71012ca3dfc85c1aeb
Douglas Maitelli <dougmaitelli@gmail.com>
2022-07-14 16:19:38 -0700

    Sync

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -6,16 +6,22 @@ if type -q exa
   alias ls "exa"
 end
 
+alias git-prune "git fetch -p ; git branch -r | awk '{print \$1}' | egrep -v -f /dev/fd/0 (git branch -vv | grep origin | psub) | awk '{print \$1}' | xargs git branch -D"
+
 if type -q jenv
   set PATH $HOME/.jenv/bin $PATH
   status --is-interactive; and source (jenv init -|psub)
 end
 
-set -gx VOLTA_HOME $HOME/.volta
-set -gx PATH $VOLTA_HOME/bin $PATH
+if [ -e $HOME/.volta ]
+  set -gx VOLTA_HOME $HOME/.volta
+  set -gx PATH $VOLTA_HOME/bin $PATH
+end
 
-set -gx PATH $PATH $HOME/.krew/bin
-set -x KUBECONFIG $HOME/.kubeconfigs/config
+if [ -e $HOME/.kubeconfigs ]
+  set -gx PATH $PATH $HOME/.krew/bin
+  set -x KUBECONFIG $HOME/.kubeconfigs/config
+end
 
 [ -e $HOME/.env ]; and source $HOME/.env
 
